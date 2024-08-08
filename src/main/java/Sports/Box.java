33bb936cd0f7a8dfa75e28afc15d429fb2200c28
Guldana <danaumar05@gmail.com>
2024-08-08 20:22:58 +0600

    football

--- src/main/java/Sports/Box.java
+++
@@ -1,7 +0,0 @@
-package Sports;
-
-public class Box {
-    public static void main(String[] args) {
-        System.out.println("Box");
-    }
-}
--- src/main/java/Sports/Golf.java
+++
@@ -1,7 +0,0 @@
-package Sports;
-
-public class Golf {
-    public static void main(String[] args) {
-        System.out.println("Golf");
-    }
-}
