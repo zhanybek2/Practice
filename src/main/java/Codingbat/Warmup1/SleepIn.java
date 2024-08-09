12b6ecfbd261aea6023c982f9981e3fddf5185f9
Guldana <danaumar05@gmail.com>
2024-08-09 10:39:38 +0600

    SleepIn

---
+++ src/main/java/Codingbat/Warmup1/SleepIn.java
@@ -0,0 +1,10 @@
+package Codingbat.Warmup1;
+
+public class SleepIn {
+    public boolean sleepIn(boolean weekday, boolean vacation) {
+        if (!weekday || vacation) {
+            return true;
+        }
+        return false;
+    }
+}
