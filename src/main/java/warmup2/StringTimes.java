f7f67d3525abfe013ee5e5bf2bfa7624cef4fbba
Guldana <danaumar05@gmail.com>
2024-08-09 10:50:54 +0600

    StrinTimes

---
+++ src/main/java/warmup2/StringTimes.java
@@ -0,0 +1,10 @@
+package warmup2;
+
+public class StringTimes {
+    public String stringTimes(String str, int n) {
+            String str2 = "";
+            for (int i = 0; i < n; i++){
+                str2 += str;
+            } return str2;
+        }
+    }
