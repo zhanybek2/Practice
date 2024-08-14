22ca5977d141bca36a592342bf28107dced9ebae
Guldana <danaumar05@gmail.com>
2024-08-14 20:39:45 +0600

    string1/ExtraFront

---
+++ src/main/java/string1/ExtraFront.java
@@ -0,0 +1,11 @@
+package string1;
+
+public class ExtraFront {
+    public String extraFront(String str) {
+        if (str.length() >= 2) {
+            return str.substring(0, 2) + str.substring(0, 2) + str.substring(0, 2);
+        } else {
+            return str + str + str;
+        }
+    }
+}
