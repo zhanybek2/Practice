ef255b07cf67e371122ea043167bbfea401ac35b
Guldana <danaumar05@gmail.com>
2024-08-14 17:49:43 +0600

    string2/counthi

---
+++ src/main/java/Codingbat/String2/CoutHi.java
@@ -0,0 +1,13 @@
+package Codingbat.String2;
+
+public class CoutHi {
+    public int countHi(String str) {
+        int count = 0;
+        for (int i = 0; i < str.length() -1; i ++){
+            if (str.substring(i, i + 2).equals("hi")){
+                count ++;
+            }
+        }
+        return count;
+    }
+}
